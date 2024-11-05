0b6c8a12c753300816186facb90436e07ed55f37
Elibar55 <87397616+Elibar55@users.noreply.github.com>
2024-11-05 00:13:03 +0000

    3rd commit

--- src/js/component/vehicleCard.js
+++ src/js/component/vehicleCard.js
@@ -4,22 +4,26 @@ import { useNavigate } from "react-router";
 
 const VehicleCard = (props) => {
     const { actions, store } = useContext(Context);
-    // Check if the current item is a favorite
     const isFavorite = store.favorites.some(fav => fav.uid === props.uid && fav.type === props.type);
-
     const navigate = useNavigate();
+
     const handleDetailsClick = () => {
         navigate(`/details/${props.type}/${props.uid}`);
     };
 
     return (
         <div className="card text-dark bg-warning m-2" style={{ minWidth: "13rem" }}>
-            <img src={`https://starwars-visualguide.com/assets/img/vehicles/${props.uid}.jpg`} className="card-img-top" alt={props.name} />
+            <img 
+                src={`https://starwars-visualguide.com/assets/img/vehicles/${props.uid}.jpg`} 
+                onError={(e) => e.target.src = "https://starwars-visualguide.com/assets/img/placeholder.jpg"} 
+                className="card-img-top" 
+                alt={props.name} 
+            />
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
-                <button className={'btn btn-outline-secondary m-2'} onClick={handleDetailsClick}>View Details</button>
+                <button className="btn btn-outline-secondary m-2" onClick={handleDetailsClick}>View Details</button>
                 <button
-                    onClick={() => actions.toggleFavorite({ uid: props.uid, name: props.name, type: props.type })} // Ensure type is passed
+                    onClick={() => actions.toggleFavorite({ uid: props.uid, name: props.name, type: props.type })}
                     className={`btn ${isFavorite ? "btn-danger" : "btn-outline-primary"}`}
                 >
                     {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
